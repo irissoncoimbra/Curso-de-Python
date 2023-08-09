cf20a04214aeb614281d8051a9a84964a720d1e3
Irisson Rodrigo <irissoncoimbra@gmail.com>
2023-08-09 11:21:25 -0300

    update

---
+++ Exercícios e Desafios/desafio1.py
@@ -0,0 +1,4 @@
+#Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado
+
+nome= input("Insira seu nome: ")
+print ('Olá,',nome,'. É um prazer te receber!')
---
+++ Exercícios e Desafios/desafio3.py
@@ -0,0 +1,6 @@
+#Crie um script python que leia dois números e tente realizar a soma entre eles
+
+n1= input('Primeiro número:')
+n2= input('Segundo número:')
+
+print ('A soma é:', n1+n2)
