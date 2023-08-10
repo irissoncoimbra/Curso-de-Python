db60576670c73fdb9b34ad5499812b4cad65178e
Irisson Rodrigo <irissoncoimbra@gmail.com>
2023-08-10 10:54:58 -0300

    update

---
+++ Operações Aritméticas/app.py
@@ -0,0 +1,14 @@
+# Adição: +
+# Subtração: -
+#Multiplicação: *
+#Divisão: /
+#Potenciação: **
+#Divisão inteira: //
+#Resto da divisão: %
+
+#ORDEM DE PRECEDÊNCIA 
+
+#1 - ()
+#2 - **
+#3 - *, /, //, %
+#4 - =, -
