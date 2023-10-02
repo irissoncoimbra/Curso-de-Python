256e52fcf061436ab10c46475997d4caa91fa2dd
Irisson Rodrigo <irissoncoimbra@gmail.com>
2023-10-02 09:02:51 -0300

    Update ex013.py

--- Exercícios e Desafios/ex013.py
+++ Exercícios e Desafios/ex013.py
@@ -1,2 +1,6 @@
 #Reajuste de salário
 salario = float(input('Digite o salário atual do contribuinte:'))
+reajuste = float(input('Digite o valor do reajuste:'))
+salarioreajustado = salario + (salario*(reajuste/100))
+
+print ('O salário atual do contribuinte é igual a R$ {:.2f}, com o reajuste de {:.0f}%, o salário passará a ser de R$ {:.2f}.' .format(salario, reajuste, salarioreajustado))
