ac6d51ef0638af12231de6fd528f1abb42023e12
Irisson Rodrigo <irissoncoimbra@gmail.com>
2023-10-02 09:18:41 -0300

    Create ex014.py

---
+++ Exercícios e Desafios/ex014.py
@@ -0,0 +1,9 @@
+#Conversor de temperaturas
+
+celsius = float(input("Digite a temperatura em Celsius:"))
+farenheit = ((celsius*9)/5)+32
+kelvin = celsius+273
+
+print ("Celsius: {}".format(celsius))
+print ('Farenheit: {}'.format(farenheit))
+print ('Kelvin: {}'.format(kelvin))
